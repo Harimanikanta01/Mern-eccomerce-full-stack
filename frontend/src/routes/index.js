a12d069da7b82583cef210753ec7671bd9d42a52
Harimanikanta01 <punugulahari1>
2026-01-13 11:29:32 +0530

    changes are added in client side

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -31,7 +31,7 @@ const router = createBrowserRouter([
         element: <ForgotPassword />,
       },
       {
-        path: "sign-up",
+        path: "register",
         element: <SignUp />,
       },
       {
